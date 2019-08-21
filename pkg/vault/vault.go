e2c1412e115e5adc13d803338f7f64a230d7fadd
Simon Macklin <simon@NOTEBOOK.local>
2019-08-21 21:20:20 +0100

    removed error module and used fmt.error instead

--- pkg/vault/vault.go
+++ pkg/vault/vault.go
@@ -3,7 +3,6 @@ package vault
 import (
 	"bytes"
 	"encoding/json"
-	"errors"
 	"fmt"
 	http "github.com/hashicorp/go-retryablehttp"
 	"io/ioutil"
@@ -18,7 +17,7 @@ type Client struct {
 func readJwt(path string) (string, error) {
 	f, err := ioutil.ReadFile(path)
 	if err != nil {
-		return "", errors.New(fmt.Sprintf("failed to read file: %s", err))
+		return "", fmt.Errorf("could not read file with jwt token: %s", err)
 	}
 	return string(bytes.TrimSpace(f)), nil
 }
@@ -38,7 +37,7 @@ func NewClient(path, address, role string) (*Client, error) {
 	body := []byte(fmt.Sprintf(`{"role": "%s", "jwt": "%s"}`, role, j))
 	resp, err := h.Post(c.address+"/v1/auth/kubernetes/login", "application/json", body)
 	if err != nil {
-		return &c, errors.New(fmt.Sprintf("fucked %s", err))
+		return &c, fmt.Errorf("failed to make the the post request to vault: %s", err)
 	}
 
 	temp := struct {
@@ -53,7 +52,7 @@ func NewClient(path, address, role string) (*Client, error) {
 
 	c.token = temp.Auth.Token
 	if c.token == "" {
-		return &c, errors.New("vault token is missing")
+		return &c, fmt.Errorf("vault token is missing or was not parsed correctly: %s", err)
 	}
 	return &c, nil
 }
@@ -92,7 +91,7 @@ func (c *Client) GetSecret(path, key string) (string, error) {
 	}
 
 	if _, ok := temp.Data.Data[key]; !ok {
-		return "", errors.New(fmt.Sprintf("no key found %s in vault path %s", key, path))
+		return "", fmt.Errorf("no key found %s in vault path %s", key, path)
 	}
 	return temp.Data.Data[key], nil
 }
